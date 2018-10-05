9570aee92833a388d17176fd0df7f249c07efe5a
Dino Morelli <dino@ui3.info>
2018-10-05 16:28:21 -0400

    Added -vv extra-verbose behavior to show the function script
    Also added more detail to the usage.

--- src/lambda/Main.hs
+++ src/lambda/Main.hs
@@ -17,20 +17,24 @@ Wrap shell commands in a lambda function
 
 Usage:
   lambda [-h]
-  lambda [-v] <COMMAND> [<ARG>] [<ARG>] ...
+  lambda [-v | -vv] <COMMAND> [<ARG>] [<ARG>] ...
 
 Options:
   -h, --help      This help info
   -v, --verbose   Echo back the command and args list before executing
+                  Pass twice to echo back the script function as well
 
 This is useful when you need to repeatedly modify an argument buried in the
 middle of a long command. lambda lets you pull the arguments out to the end of
 the command line where it's easier to edit them. It can also be used as a
 `flip` function to reorder arguments.
 
+Use the customary $1, $2... variables. Quoting can be difficult. For debugging
+your commands, put 'echo ' in front of them and try running with --verbose
+
 Example:
 
-  $ lambda 'find $2 -name "$1"' *.log /var/log
+  $ lambda 'find $2 -name $1' *.log /var/log
 
 The exit code will be that of COMMAND
 |]
@@ -46,12 +50,16 @@ main = do
 
   let userArgs = unwords $ DO.getAllArgs args (DO.argument "ARG")
 
+  let script = printf "function lambdaf() { %s; }; lambdaf %s"
+        userCommand userArgs
+
   when (DO.isPresent args $ DO.longOption "verbose") $ do
     putStrLn $ "COMMAND: " ++ userCommand
     putStrLn $ "ARGS: " ++ userArgs
+    when ((DO.getArgCount args $ DO.longOption "verbose") > 1) $ do
+      putStrLn $ "SCRIPT: " ++ script
+    putStrLn ""
 
-  let script = printf "function lambdaf() { %s; }; lambdaf %s"
-        userCommand userArgs
   exitCode <- system script
   exitWith exitCode
 
