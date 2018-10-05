1dc3e84da2b236ecfa60a436ea574bdb930c2dc5
Dino Morelli <dino@ui3.info>
2018-10-05 10:41:14 -0400

    Trying out docopt, TH version

--- src/lambda/Main.hs
+++ src/lambda/Main.hs
@@ -1,12 +1,41 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+import Control.Monad ( when )
+import System.Console.Docopt
 import System.Environment ( getArgs )
-import System.Exit ( exitWith )
+import System.Exit ( exitSuccess, exitWith )
 import System.Process ( system )
 import Text.Printf ( printf )
 
 
+patterns :: Docopt
+patterns = [docopt|
+lambda v1.0
+
+Usage:
+  lambda [-h]
+  lambda [-v] <COMMAND> [<ARG>] [<ARG>] ...
+
+Options:
+  -h, --help      This help info
+  -v, --verbose   Be verbose
+|]
+
+getArgOrExit = getArgOrExitWith patterns
+
+
 main :: IO ()
 main = do
-  (command : args) <- getArgs
+  args <- parseArgsOrExit patterns =<< getArgs
+  print args
+
+  when (isPresent args (longOption "help")) $ do
+    putStrLn $ usage patterns
+    exitSuccess
+
+  --(command : args) <- getArgs
+  print $ getArg args (argument "COMMAND")
+  print $ getAllArgs args (argument "ARG")
 
   {- For debugging
   putStr "Command: "
@@ -15,6 +44,8 @@ main = do
   print . unwords $ args
   -}
 
+  {-
   let script = printf "function lambdaf() { %s; }; lambdaf %s" command (unwords args)
   exitCode <- system script
   exitWith exitCode
+  -}
