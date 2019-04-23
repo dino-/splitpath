20258349502116fea356480686ba4784d729f2a7
Dino Morelli <dino@ui3.info>
2019-04-23 17:18:16 -0400

    Now using optparse-applicative instead of Docopt
    This library has allowed us to model with much stronger types, and
    handle that all of the switches are mutually-exclusive. It also handles
    help and the positional argument for us with strong typing.
    
    Another benefit is that all of the error handling code is gone from the
    Main module.
    
    Closes gh-1
    Closes gh-2
    Closes gh-6

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -1,89 +1,26 @@
-{-# LANGUAGE QuasiQuotes #-}
-
-import Control.Monad ( when )
-import System.Console.Docopt (
-  Arguments, Docopt,
-  argument, docopt, getArg, isPresent,
-  longOption, parseArgsOrExit, usage )
-import System.Environment ( getArgs )
-import System.Exit ( exitFailure, exitSuccess )
 import System.FilePath (
   dropExtension, dropExtensions, dropFileName, takeBaseName,
   takeDirectory, takeExtension, takeExtensions, takeFileName )
-import System.IO ( hPutStrLn, stderr )
 
-
-patterns :: Docopt
-patterns = [docopt|
-Comprehensive path splitting utility
-
-Usage:
-  splitpath [options] <PATH>
-
-Options:
-  --takedirectory     Get the directory name (move up one level)
-  --dropfilename      Drop the filename, leave the trailing path separator
-  --takeextension     Get the extension
-  --dropextension     Drop the extension
-  --takeextensions    Get all extensions
-  --dropextensions    Drop all extensions
-  --takebasename      Get the base name, without an extention or path
-  --takefilename      Get the file name
-  -h, --help          This help info
-
-The standard tools for separating file paths in bash are primitive and lacking
-features. There are also problems when spaces are present in the path. On the
-other hand, many programming language standard libraries have powerful tools
-for breaking apart file paths. This utility wraps the functionality present in
-Haskell's System.FilePath library, exposing this behavior as a simple utility
-with switches.
-
-Examples:
-
-  $ splitpath --takedirectory  /foo/bar/baz.tar.gz  # /foo/bar
-  $ splitpath --dropfilename   /foo/bar/baz.tar.gz  # /foo/bar/
-  $ splitpath --takeextension  /foo/bar/baz.tar.gz  # .gz
-  $ splitpath --dropextension  /foo/bar/baz.tar.gz  # /foo/bar/baz.tar
-  $ splitpath --takeextensions /foo/bar/baz.tar.gz  # .tar.gz
-  $ splitpath --dropextensions /foo/bar/baz.tar.gz  # /foo/bar/baz
-  $ splitpath --takebasename   /foo/bar/baz.tar.gz  # baz.tar
-  $ splitpath --takefilename   /foo/bar/baz.tar.gz  # baz.tar.gz
-
-
-Version 1.0  Dino Morelli <dino@ui3.info>
-|]
+import Options
+  ( Options (DropExtension, DropExtensions, DropFilename,
+      TakeBasename, TakeDirectory, TakeExtension,
+      TakeExtensions, TakeFilename, Version),
+    parseOptions, versionInfo
+  )
 
 
 main :: IO ()
-main = do
-  args <- parseArgsOrExit patterns =<< getArgs
-  handleHelp args
-
-  path <- maybe (exitWithMsg "ERROR: PATH required") return
-    $ getArg args (argument "PATH")
-
-  either exitWithMsg putStrLn $ extractPart args path
+main = parseOptions >>= putStrLn . extractPart
 
 
-handleHelp :: Arguments -> IO ()
-handleHelp args =
-  when (isPresent args $ longOption "help") $ do
-    putStrLn $ usage patterns
-    exitSuccess
-
-
-exitWithMsg :: String -> IO a
-exitWithMsg msg = hPutStrLn stderr msg >> exitFailure
-
-
-extractPart :: Arguments -> FilePath -> Either String FilePath
-extractPart args path
-  | isPresent args $ longOption "takedirectory" = Right . takeDirectory $ path
-  | isPresent args $ longOption "dropfilename" = Right . dropFileName $ path
-  | isPresent args $ longOption "takeextension" = Right . takeExtension $ path
-  | isPresent args $ longOption "dropextension" = Right . dropExtension $ path
-  | isPresent args $ longOption "takeextensions" = Right . takeExtensions $ path
-  | isPresent args $ longOption "dropextensions" = Right . dropExtensions $ path
-  | isPresent args $ longOption "takebasename" = Right . takeBaseName $ path
-  | isPresent args $ longOption "takefilename" = Right . takeFileName $ path
-  | otherwise = Left "ERROR: An option must be specified"
+extractPart :: Options -> FilePath
+extractPart (TakeDirectory  path) = takeDirectory  path
+extractPart (DropFilename   path) = dropFileName   path
+extractPart (TakeExtension  path) = takeExtension  path
+extractPart (DropExtension  path) = dropExtension  path
+extractPart (TakeExtensions path) = takeExtensions path
+extractPart (DropExtensions path) = dropExtensions path
+extractPart (TakeBasename   path) = takeBaseName   path
+extractPart (TakeFilename   path) = takeFileName   path
+extractPart Version               = versionInfo
---
+++ src/app/Options.hs
@@ -0,0 +1,111 @@
+{-# LANGUAGE QuasiQuotes #-}
+
+module Options
+  ( Options (..)
+  , parseOptions
+  , versionInfo
+  )
+  where
+
+import Data.Version ( showVersion )
+import Options.Applicative
+import Paths_splitpath ( version )
+import Text.Heredoc ( here )
+import Text.PrettyPrint.ANSI.Leijen ( string )
+import Text.Printf ( printf )
+
+
+data Options
+  = TakeDirectory FilePath
+  | DropFilename FilePath
+  | TakeExtension FilePath
+  | DropExtension FilePath
+  | TakeExtensions FilePath
+  | DropExtensions FilePath
+  | TakeBasename FilePath
+  | TakeFilename FilePath
+  | Version
+
+
+parser :: Parser Options
+parser =
+  TakeDirectory <$> strOption
+    (  long "takedirectory"
+    <> metavar "PATH"
+    <> help "Get the directory name (move up one level)"
+    ) <|>
+  DropFilename <$> strOption
+    (  long "dropfilename"
+    <> metavar "PATH"
+    <> help "Drop the filename, leave the trailing path separator"
+    ) <|>
+  TakeExtension <$> strOption
+    (  long "takeextension"
+    <> metavar "PATH"
+    <> help "Get the extension"
+    ) <|>
+  DropExtension <$> strOption
+    (  long "dropextension"
+    <> metavar "PATH"
+    <> help "Drop the extension"
+    ) <|>
+  TakeExtensions <$> strOption
+    (  long "takeextensions"
+    <> metavar "PATH"
+    <> help "Get all extensions"
+    ) <|>
+  DropExtensions <$> strOption
+    (  long "dropextensions"
+    <> metavar "PATH"
+    <> help "Drop all extensions"
+    ) <|>
+  TakeBasename <$> strOption
+    (  long "takebasename"
+    <> metavar "PATH"
+    <> help "Get the base name, without an extention or path"
+    ) <|>
+  TakeFilename <$> strOption
+    (  long "takefilename"
+    <> metavar "PATH"
+    <> help "Get the file name"
+    ) <|>
+  Version <$ switch
+    (  long "version"
+    <> help "Display version"
+    )
+
+
+parseOptions :: IO Options
+parseOptions = execParser $ info (parser <**> helper)
+  (  header "splitpath - Comprehensive path splitting utility"
+  <> footer'
+  )
+
+
+footer' :: InfoMod a
+footer' = footerDoc . Just . string $ printf content (showVersion version)
+  where content = tail . init $ [here|
+The standard tools for separating file paths in bash are primitive and lacking
+features. There are also problems when spaces are present in the path. On the
+other hand, many programming language standard libraries have powerful tools
+for breaking apart file paths. This utility wraps the functionality present in
+Haskell's System.FilePath library, exposing this behavior as a simple utility
+with switches.
+
+Examples:
+
+  $ splitpath --takedirectory  /foo/bar/baz.tar.gz  # /foo/bar
+  $ splitpath --dropfilename   /foo/bar/baz.tar.gz  # /foo/bar/
+  $ splitpath --takeextension  /foo/bar/baz.tar.gz  # .gz
+  $ splitpath --dropextension  /foo/bar/baz.tar.gz  # /foo/bar/baz.tar
+  $ splitpath --takeextensions /foo/bar/baz.tar.gz  # .tar.gz
+  $ splitpath --dropextensions /foo/bar/baz.tar.gz  # /foo/bar/baz
+  $ splitpath --takebasename   /foo/bar/baz.tar.gz  # baz.tar
+  $ splitpath --takefilename   /foo/bar/baz.tar.gz  # baz.tar.gz
+
+Version %s  Dino Morelli <dino@ui3.info>
+|]
+
+
+versionInfo :: String
+versionInfo = "splitpath " ++ showVersion version
